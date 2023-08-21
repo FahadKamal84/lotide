ae5c24cac2e16c52f2ddb59ddcadc6ba2906772e
Fahad Kamal <fahadkamal@hotmail.com>
2023-08-21 17:01:30 -0400

    function to test whether two values are equal Replacement for consoleassert

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// Function Implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🙂Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🙁Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// Test code
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
