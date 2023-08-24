c2324fb6603e52ccd601020b7e959b14e79607a0
Fahad Kamal <fahadkamal@hotmail.com>
2023-08-23 20:27:03 -0400

    first commit

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🙂Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🙁Assertion Failed: ${actual} !== ${expected}`);
+  };
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function (tvshowsgenre, tvshow) {
+  let result = "";
+  const test = Object.keys(tvshowsgenre);
+  for (let shows of test) {
+    //console.log(shows);
+    if (tvshowsgenre[shows] === tvshow) { 
+      return shows;
+    }
+  }
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
+//assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+//assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
