587564e145a8b638a19788f17d6eddfcee938cd8
Fahad Kamal <fahadkamal@hotmail.com>
2023-08-28 13:40:46 -0400

    refactoring files to make them DRY instead of WET code, by modularizing them

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🙂Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🙁Assertion Failed: ${actual} !== ${expected}`);
-  };
-}
+const assertEqual = require('./assertEqual');
 
 const head = function(testArray) {
   let firstElement = 0;
@@ -12,7 +6,4 @@ const head = function(testArray) {
   return firstElement;
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([]),)
+module.exports = head;
