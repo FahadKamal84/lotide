d7ab84bcdb9b81545efc7c3a182d2cf77cec535e
Fahad Kamal <fahadkamal@hotmail.com>
2023-08-28 17:18:46 -0400

    commented out test code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,12 +8,12 @@ const assertArraysEqual = function(actual, expected) {
   };
 };
 
-module.export = assertArraysEqual;
+module.exports = assertArraysEqual;
 
 
 ///TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+/*assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
 assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
 
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false*/
