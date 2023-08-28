62fb06fd504e39b176eead0a70dfb7c0b27328bb
Fahad Kamal <fahadkamal@hotmail.com>
2023-08-28 15:32:29 -0400

    refactoring files to make them DRY instead of WET code, by modularizing them

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🙂Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🙁Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arrayOne, arrayTwo) {
   if (arrayOne.length === arrayTwo.length) {
     for (let i = 0; i < arrayOne.length; i++) {
@@ -19,13 +11,4 @@ const eqArrays = function(arrayOne, arrayTwo) {
   }
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
